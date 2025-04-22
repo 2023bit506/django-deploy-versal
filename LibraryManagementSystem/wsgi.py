18baaa902b3e8f6eaeba655fb4d3a141325eb0bc
2023bit506 <2023bit506@sggs.ac.in>
2025-04-22 21:18:26 +0530

    update

--- LibraryManagementSystem/wsgi.py
+++ LibraryManagementSystem/wsgi.py
@@ -1,18 +1,11 @@
-"""
-WSGI config for LibraryManagementSystem project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryManagementSystem.settings')
 
+# Get the WSGI application
 application = get_wsgi_application()
 
+# Vercel looks for 'app' as the entry point
 app = application
